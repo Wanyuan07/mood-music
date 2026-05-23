11d4ec8bc7e1724ef32dd8ad1fe9ac5217cea248
Priyanshi <priyanshi.sah07@gmail.com>
2026-05-23 18:43:46 +0530

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,5 +1,5 @@
 let storage = localStorage.getItem("savedMusic");
-let music = JSON.parse(storage);
+let music = JSON.parse(storage); 
 if (!music || music.length == 0) {
     document.querySelector(".ini_msg").innerHTML = "Save songs to view this page...";
 }
