448ffb8c3cc02927805a0c9956fdc63170edef9f
Priyanshi <priyanshi.sah07@gmail.com>
2026-05-28 12:37:01 +0530

    Update playlist.js
    Added search functionality to find songs in the playlist.

--- playlist.js
+++ playlist.js
@@ -1,5 +1,9 @@
 let storage = localStorage.getItem("savedMusic");
 let music = JSON.parse(storage);
+console.log(music);
+console.log(typeof(music));
+console.log(music.length);
+
 if (!music || music.length == 0) {
     document.getElementById("ini_msg").style.display = "block";
     document.getElementById("part").style.display = "none";
@@ -9,6 +13,7 @@ else {
     music.forEach(song => fav(song))
 }
 
+//------------------------------------------------------------------
 
 function fav(song) {
     let songData = {
@@ -40,6 +45,7 @@ function fav(song) {
     })
 }
 
+//------------------------------------------------------------------
 
 let theme_img = document.querySelector(".theme-img");
 let nav_bar = document.querySelector(".nav-bar");
@@ -65,4 +71,46 @@ document.getElementById("theme").addEventListener("click", function() {
     localStorage.setItem("theme", JSON.stringify(dark));
 })
 
+//------------------------------------------------------------------
 
+let input = document.getElementById("searchbox");
+let btn =  document.getElementById("searchbtn");
+btn.addEventListener("click", function() {
+    let search = input.value.toLowerCase();
+    value(search);
+})
+input.addEventListener("keydown", function(event) {
+    if(event.key == "Enter") {
+        let search = input.value.toLowerCase();
+        value(search);
+    }
+})
+
+//------------------------------------------------------------------
+
+function value(search) {
+    search = search.split(" ");
+    let key_word = music.filter(song => search.some(word => song.name.toLowerCase().split(" ").includes(word)));
+    console.log(key_word);
+    if (key_word.length > 0) {
+        input.value = "";
+        document.querySelector(".msg").innerHTML = `<button class="back-btn">
+                                                        <img src="./images/back.png" class="back-img">
+                                                    </button> 
+                                                    Your search result....`;
+        document.getElementById("container").innerHTML = "";
+        document.querySelector(".back-btn").addEventListener("click", function() {
+            window.location.href = "./playlist.html";
+        })
+        fav(key_word[0]);
+    }
+    else {
+        input.value = "";
+        document.getElementById("ini_msg").style.display = "block";
+        document.getElementById("part").style.display = "none";
+        document.getElementById("ini_msg").innerHTML = "No such song found....";
+        setTimeout(() => {
+            window.location.href = "./playlist.html";
+        }, 2000);
+    }
+}
